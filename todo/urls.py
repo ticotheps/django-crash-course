c2b6182c181efae843a4107674c94e59ea515d97
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-07-01 11:13:38 -0400

    feat(urls): create a path for HTTP request sent to the 'update/<id>/' url

--- todo/urls.py
+++ todo/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import todo_list, todo_create, todo_read
+from .views import todo_list, todo_create, todo_read, todo_update
 
 app_name = "todos"
 
 urlpatterns = [
     path('', todo_list),
     path('create/', todo_create),
+    path('update/<id>/', todo_update),
     path('<id>/', todo_read)
 ]
