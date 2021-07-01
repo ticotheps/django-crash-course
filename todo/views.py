60f1c654c9334d94d02ea693b79844ee599cd3bc
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-07-01 10:27:48 -0400

    feat(views): create a new view that handles GET requests to '/<id>/'
    This view will retrieve todo items based on id.

--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Todo
 
+# generates full list of all todo items
 def todo_list(request):
     todos = Todo.objects.all()
     print(todos)
@@ -9,3 +10,11 @@ def todo_list(request):
         "todo_list": todos
     }
     return render(request, "todo_list.html", context)
+
+# Read any todo item based on id
+def todo_read(request, id):
+    todo = Todo.objects.get(id=id)
+    context = {
+        "todo": todo
+    }
+    return render(request, "todo_read.html", context)
