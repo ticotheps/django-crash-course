92d186fb41cbf8bc99a13053ed9fd55a64fd3bab
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-07-01 11:03:26 -0400

    refactor(views): convert all todo creation logic into one-liner
    This one-liner abstracts away all of the logic behind creating a new todo object because it uses Django's 'forms' model.

--- todo/views.py
+++ todo/views.py
@@ -18,13 +18,14 @@ def todo_create(request):
     # populates the 'TodoForm' only if the HTTP request is a POST request.
     form = TodoForm(request.POST or None)
     if form.is_valid():
-        # print(form.cleaned_data)
-        name = form.cleaned_data['name']
-        due_date = form.cleaned_data['due_date']
-        print(name, due_date)
+        # # print(form.cleaned_data)
+        # name = form.cleaned_data['name']
+        # due_date = form.cleaned_data['due_date']
+        # print(name, due_date)
         
-        # create a new todo object
-        new_todo = Todo.objects.create(name=name, due_date=due_date)
+        # # create a new todo object
+        # new_todo = Todo.objects.create(name=name, due_date=due_date)
+        form.save()
 
     context = {
         "form": form
